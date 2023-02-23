59aa565f6f93c3580245924cbff0991c33a67312
nabaldwin <nabaldwin@gmail.com>
2023-02-22 20:10:11 -0700

    lights turn on/off in sequence

--- arrayTest.ino
+++ arrayTest.ino
@@ -9,10 +9,13 @@ const int cNum = 1;
 const int dNum = 1;
 const int eNum = 1;
 const int fNum = 1;
+
 unsigned long lastTime;
 
 const int NUM_LETTERS = 6;
-  int letters[NUM_LETTERS][3] = {
+
+//each row is one letters rgb value 
+  int letterColors[NUM_LETTERS][3] = {
     {
       255, 0, 0
     },
@@ -33,14 +36,19 @@ const int NUM_LETTERS = 6;
     }  
   };
   
-//  int letterOn[NUM_LETTERS] = {1, 1, 1, 1, 1, 1};
+  //letterOn  - 0 = off , 1 == on
+  int letterOn[NUM_LETTERS] = {0, 0, 0, 0, 0, 0};
+  //letterFade range from 0 to 255 where 0 is brightest
   int letterFade[NUM_LETTERS] = {0, 0, 0, 0, 0, 0};
+  //Number of leds for each letter 
   int letterLedNum[NUM_LETTERS] = {aNum, bNum, cNum, dNum, eNum, fNum};
   
 
+  int counter = 0;
+  int currentLetter = 0;
 
 CRGB leds[NUM_LEDS];
-
+int singleFadeValue;
 
 
 
@@ -59,27 +67,44 @@ void setup() {
 void loop() {
     lastTime = millis();
 
-    for(int i = 0; i < 3; i++){
-      if(letterFade[i] >= 255) {
-        letterFade[i] = 0;
-      } else {
-        letterFade[i]= letterFade[i]+1;
-      }
-    }
+
+  
+    // for(int k = 0; k < NUM_LETTERS; k++){
+    //   // Serial.println(k);
+    //   if(letterFade[k] >= 255) {
+    //     letterFade[k] = 0;
+    //   } else {
+    //     singleFadeValue = letterFade[k];
+    //     letterFade[k]= singleFadeValue+1;
+      
+    //   }
+    //   // delay(10);
+    //   // Serial.println(letterFade[k]);
+    // }
+
+
     
-    delay(100);
-    writeByArray(letterLedNum, letters, letterFade);
     
+    
+    //currentLetter to turn on or off
+    currentLetter = counter%NUM_LETTERS;
+    //current letter will go to opposite state on/off
+    letterOn[currentLetter] = !letterOn[currentLetter];
+        
+    
+     writeByArray(letterLedNum, letterColors, letterFade, letterOn);
+     //Delay determines how quickly move betweent letters
+     delay(500);
+    counter++;
  }
 
 
-void writeByArray( int ledNum[], int letterColors[NUM_LETTERS][3], int fadeValue[]){
+
+void writeByArray( int ledNum[NUM_LETTERS], int colors[NUM_LETTERS][3], int fadeValue[NUM_LETTERS], int on[NUM_LETTERS]){
      //elemet in arrays represent one letter
     //on[] = set to zero for letter to be off or one for letter to be on
     //ledNum[] = number of leds in letter
-    //rValue  red color value for each letter
-    //gValue green color value for each letter
-    //bValue blue value for each letter
+    //colors = coller for each letter  r
     //fadeValue = fadvalue for each letter
     int startValue = 0;
     int endValue = 0;
@@ -88,15 +113,23 @@ void writeByArray( int ledNum[], int letterColors[NUM_LETTERS][3], int fadeValue
         
        endValue = startValue+ledNum[i];
 
-        for(int j = startValue; j <= endValue; j++) {
+        for(int j = startValue; j < endValue; j++) {
             
-            leds[j] = CRGB(letterColors[i][0], letterColors[i][1], letterColors[i][2]);
-            leds[j].fadeToBlackBy(fadeValue[i]);
+            leds[j] = CRGB(colors[i][0]*on[i], colors[i][1]*on[i], colors[i][2]*on[i]);
+            singleFadeValue = fadeValue[i];
+            leds[j].fadeToBlackBy(singleFadeValue);
             
         }
-        Serial.println(fadeValue[i]);
+        if (i==1){
+                  
+                  // Serial.println(fadeValue[i]);
+
+
+        }
         startValue = endValue;
     }
     FastLED.show();  
 
+
 }
+
